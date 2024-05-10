3dd51e22f92d51caa82c66783f2e0d7c80103203
Michael Rowsey <mike@rowsey.org>
2024-05-10 02:08:58 -0700

    add tokyo night in it's own block

--- init.lua
+++ init.lua
@@ -774,13 +774,17 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'folke/tokyonight.nvim',
+    name = 'tokyonight',
+    priority = 1000,
+  },
 
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    -- 'folke/tokyonight.nvim',
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
